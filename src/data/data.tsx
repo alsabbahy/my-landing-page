2b2caf93f45f121756d88371c5bc28bfbf9efdd4
alsabbahy <ahmed.alsabbahy@gmail.com>
2025-02-08 13:22:39 +0200

    Update profile image and styling
    - Replace profile picture with new image
    - Change profile image container to rounded-full with outline-double
    - Update profilepic import in data.tsx to use new image file

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -29,7 +29,7 @@ import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
 import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
 import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
 import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
-import profilepic from '../images/profilepic.jpg';
+import profilepic from '../images/profilepic-2.png';
 import testimonialImage from '../images/testimonial.webp';
 // Type imports
 import {
